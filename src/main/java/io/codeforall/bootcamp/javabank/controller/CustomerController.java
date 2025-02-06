3a955599a262af751586eb0daa88cb77b21fe513
Rogério Freitas <rogerio.m.freitas@ctt.pt>
2025-02-06 11:52:11 +0000

    final

--- src/main/java/io/codeforall/bootcamp/javabank/controller/CustomerController.java
+++ src/main/java/io/codeforall/bootcamp/javabank/controller/CustomerController.java
@@ -1,23 +1,26 @@
 package io.codeforall.bootcamp.javabank.controller;
 
 import io.codeforall.bootcamp.javabank.services.CustomerService;
-import io.codeforall.bootcamp.javabank.services.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class CustomerController {
 
     private CustomerService customerService;
-    public Controller(CustomerService customerService){
+
+    @Autowired
+    public void setCustomerController(CustomerService customerService){
         this.customerService=customerService;
     }
 
 
     // Map the URL/Verb to the method
     @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String control(Model model) {
+    public String showCustomer(Model model) {
 
 // Add an attribute to the request
         model.addAttribute("customers",customerService.getCustomers());
